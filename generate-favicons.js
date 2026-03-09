61448959dceb1eeaf42821c0313154cbde6d30a1
akash dey <dey3552@gmail.com>
2026-03-09 21:02:41 +0530

    logo update2

--- generate-favicons.js
+++ generate-favicons.js
@@ -8,17 +8,17 @@ async function generateFavicons() {
         // Standard Favicon 32x32
         await sharp(source)
             .resize(32, 32)
-            .toFile('public/favicon-32x32.png');
+            .toFile('public/image/logo2.png');
 
         // Recommended for Google Search Results (multiple of 48)
         await sharp(source)
             .resize(48, 48)
-            .toFile('public/favicon-48x48.png');
+            .toFile('public/image/logo2.png');
 
         // Apple Touch Icon
         await sharp(source)
             .resize(180, 180)
-            .toFile('public/apple-touch-icon.png');
+            .toFile('public/image/logo2.png');
 
         console.log("Favicons generated successfully.");
     } catch (error) {
